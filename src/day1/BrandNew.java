8943fee59bf8e991b300515c44bbe9e10b83d718
esmaAkcagun <62145736+esmaAkcagun@users.noreply.github.com>
2020-05-13 15:00:46 +0200

    Update BrandNew.java

--- src/day1/BrandNew.java
+++ src/day1/BrandNew.java
@@ -3,5 +3,8 @@ package day1;
 public class BrandNew {
     public static void main(String[] args) {
         System.out.println("13 may 2020");
+        
+        //today is wednesday
+        
     }
 }
